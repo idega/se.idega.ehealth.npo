2ca7b77161bca5532bace14aab3df845ee0789b2
Maris Orbidans <Maris.Orbidans@idega.lv>
2005-10-26 15:46:41 +0000

    *** empty log message ***

--- src/java/se/idega/ehealth/npo/constants/Constants.java
+++ src/java/se/idega/ehealth/npo/constants/Constants.java
@@ -2,7 +2,7 @@ package se.idega.ehealth.npo.constants;
 
 public class Constants
 {
-	public static final String NPO_PROPERTIES_FILENAME="/nposervice.properties";
+	public static final String NPO_PROPERTIES_FILENAME="/se/idega/ehealth/npo/config/nposervice.properties";
 	
 	// properties
 	public static final String SESSION_TIME_OUT_PROPERTY = "http_session_timeout";
